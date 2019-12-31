425ee425002ba61f7d19c3660a7885472fafc128
Allen Lucke <allenlucke@gmail.com>
2019-12-31 14:22:45 -0600

    redux folder, rootReducer, and rootSaga created

---
+++ src/redux/sagas/_root.saga.js
@@ -0,0 +1,15 @@
+import { all } from 'redux-saga/effects';
+import { takeEvery} from 'redux-saga/effects';
+//Imported sagas
+
+
+//Watcher Saga
+function* rootSaga() {
+    yield all(
+        [
+
+        ]
+    );
+}
+
+export default rootSaga;

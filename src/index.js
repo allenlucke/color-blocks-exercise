f09260a8a073f366cdbf6e15012239d2c150f257
Allen Lucke <allenlucke@gmail.com>
2019-12-31 12:04:08 -0600

    fixed app pathing, added dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+// Provider allows us to use redux within our react app
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

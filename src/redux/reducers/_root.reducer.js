28280cf21c8ca1202d2c8c6d55fed19f8636d488
Allen Lucke <allenlucke@gmail.com>
2020-01-02 12:52:42 -0600

    added getBlocksReducer

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 //Reducers
+import getBlocksReducer from './getBlocks.reducer';
 
 const rootReducer = combineReducers({
-
+    getBlocksReducer,
 })
 export default rootReducer;

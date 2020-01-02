1212ed2b656e765584deec910b10c197cfa78da7
Allen Lucke <allenlucke@gmail.com>
2020-01-02 12:47:03 -0600

    added getBlocksSaga

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -1,13 +1,15 @@
 import { all } from 'redux-saga/effects';
 import { takeEvery} from 'redux-saga/effects';
 //Imported sagas
-
+import getBlocksSaga from './getBlocks.saga';
 
 //Watcher Saga
 function* rootSaga() {
+    yield takeEvery('GET_BLOCKS', getBlocksSaga)
+    console.log('In root saga')
     yield all(
         [
-
+            getBlocksSaga(),
         ]
     );
 }

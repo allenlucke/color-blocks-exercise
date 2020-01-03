43cc8065315616f1d98483876fa46c30c3018203
Allen Lucke <allenlucke@gmail.com>
2020-01-03 13:34:24 -0600

    changed conditional rendering in swatcges.js to utilize item.hex_code

--- src/components/pages/swatches/swatches.js
+++ src/components/pages/swatches/swatches.js
@@ -11,6 +11,9 @@ class Swatches extends Component {
         this.props.dispatch({
             type: 'GET_BLOCKS'
         })
+        this.props.dispatch({
+            type: 'GET_COLORS'
+        })
     }
     
     deleteBlock = (event, id) => {
@@ -24,22 +27,12 @@ class Swatches extends Component {
     }
 
     render() {
-        let el;
         const colorBlocks = this.props.reduxState.getBlocksReducer.map((item, index) => {
-            if (item.hex_code === 'ff0000') {
-                el =<div className="box red" ></div>;
-            } else if(item.hex_code === '00ff00') {
-                el =<div className="box green" ></div>;
-            } else if(item.hex_code === '0000ff') {
-                el =<div className="box blue" ></div>;
-            }else if(item.hex_code === 'ffff00') {
-                el =<div className="box yellow" ></div>;
-            };
+            const el = `#${item.hex_code}`
+            const block = <div style= {{backgroundColor: el }} className="box"></div>;
             return(
                 <div key={index} className="swatchesBody">
-                    {/* <h4>{item.id}</h4>
-                    <h4>{item.hex_code}</h4> */}
-                    {el}
+                    {block}
                     <h4 className="label">{item.label}</h4>
                     <div className="label">
                         <button onClick={(event) =>this.deleteBlock(event, item.id)}>DELETE</button>
@@ -47,8 +40,21 @@ class Swatches extends Component {
                 </div>
             )
         })
+        const colorButtons = this.props.reduxState.getColorsReducer.map((item, index) => {
+            const el = `#${item.hex_code}`
+            console.log(el)
+            return(
+                <div key={index} className="swatchesBody">
+                    <button 
+                    className="colorButton"
+                    style= {{backgroundColor: el }}
+                    >Add a {item.label} Box</button>
+                </div>
+            )
+        })
         return (
             <div >
+                {colorButtons}
                 <h2>Color Swatches</h2>
                 {colorBlocks}
             </div>
